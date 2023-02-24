1f31809cebf47b6a4c144441454df3a0af5089cd
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-02-24 20:10:36 +0300

    commit

--- clone-website-1/src/components/MainBottom.vue
+++ clone-website-1/src/components/MainBottom.vue
@@ -1,51 +1,62 @@
 <template>
-    <div class="w-full flex flex-wrap gap-9 justify-between items-center">
-
-        <div class="bg-gray-900 w-full text-white p-9 flex flex-col gap-7 rounded-xl items-center justify-center">
-            <h2 class="font-bold text-3xl">Where connected teams ship better products, faster</h2>
-            <button class="bg-gray-100 w-fit text-black py-4 px-6 rounded-full text-lg font-semibold">Try the beta
-                free</button>
-            <p class="text-xl">No credit card. No learning curve. No barriers. No limits. You’ll be up and running within
-                minutes.</p>
-        </div>
-
-        <div class="w-full md:w-96 bg-gray-200 py-12 text-left px-8 rounded-xl">
-            <h2 class="text-3xl font-bold">We're hiring</h2><br>
-            <p class="text-lg">We’re making a lightning fast and super-delightful product.
-
-                If you want to help, <span class="underline font-bold">email us.</span> </p>
-        </div>
-
-        <div class="w-full md:w-96 bg-gray-200 py-12 text-left px-8 rounded-xl">
-            <h2 class="text-3xl font-bold">Contact us</h2><br>
-            <p class="text-lg">Send an email to <span class="underline font-bold">contact@superthread.com</span> and we’ll
-                get back to you as soon as possible.
-                Or join our <span class="underline font-bold">Slack</span> community. </p>
-        </div>
+  <div class="flex w-full flex-wrap items-center justify-between gap-9">
+    <div
+      class="flex w-full flex-col items-center justify-center gap-7 rounded-xl bg-gray-900 p-9 text-white"
+    >
+      <h2 class="text-3xl font-bold">Where connected teams ship better products, faster</h2>
+      <button class="w-fit rounded-full bg-gray-100 py-4 px-6 text-lg font-semibold text-black">
+        Try the beta free
+      </button>
+      <p class="text-xl">
+        No credit card. No learning curve. No barriers. No limits. You’ll be up and running within
+        minutes.
+      </p>
     </div>
 
-    <footer class="flex flex-wrap justify-between items-center px-16 py-9">
-        <div>
-            <ul>
-                <li class="text-xl font-semibold text-gray-500">Product</li>
-                <li class="text-xl mt-3 font-semibold">Changelog</li>
-                <li class="text-xl mt-3 font-semibold">Knowlage base</li>
-                <li class="text-xl mt-3 font-semibold">&#9752; Slack communuty</li>
-            </ul>
-        </div>
-        <div>
-            <ul>
-                <li class="text-xl font-semibold text-gray-500">Company and legal</li>
-                <li class="text-xl mt-3 font-semibold">&#9743; &#9742; &#9752;</li>
-                <li class="text-xl mt-3 font-semibold">Privacy policy</li>
-                <li class="text-xl mt-3 font-semibold">Term of service</li>
-            </ul>
-        </div>
-        <div class="w-full text-center flex flex-col justify-center items-center mt-4">
-            <img class="w-1/3"
-                src="../images/icons/63c7dc03818935e8280a6692_superthread-logo.svg" alt="">
-            <p class="text-lg"> &copy; 2020 - 2023. All rights reserved.</p>
-            </div>
+    <div class="w-full rounded-xl bg-gray-200 py-12 px-8 text-left md:w-96">
+      <h2 class="text-3xl font-bold">We're hiring</h2>
+      <br />
+      <p class="text-lg">
+        We’re making a lightning fast and super-delightful product. If you want to help,
+        <span class="font-bold underline">email us.</span>
+      </p>
+    </div>
 
-    </footer>
+    <div class="w-full rounded-xl bg-gray-200 py-12 px-8 text-left md:w-96">
+      <h2 class="text-3xl font-bold">Contact us</h2>
+      <br />
+      <p class="text-lg">
+        Send an email to <span class="font-bold underline">contact@superthread.com</span> and we’ll
+        get back to you as soon as possible. Or join our
+        <span class="font-bold underline">Slack</span> community.
+      </p>
+    </div>
+  </div>
+
+  <footer class="flex flex-wrap items-center justify-between px-16 py-9">
+    <div>
+      <ul>
+        <li class="text-xl font-semibold text-gray-500">Product</li>
+        <li class="mt-3 text-xl font-semibold">Changelog</li>
+        <li class="mt-3 text-xl font-semibold">Knowlage base</li>
+        <li class="mt-3 text-xl font-semibold">&#9752; Slack communuty</li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li class="text-xl font-semibold text-gray-500">Company and legal</li>
+        <li class="mt-3 text-xl font-semibold">&#9743; &#9742; &#9752;</li>
+        <li class="mt-3 text-xl font-semibold">Privacy policy</li>
+        <li class="mt-3 text-xl font-semibold">Term of service</li>
+      </ul>
+    </div>
+    <div class="mt-4 flex w-full flex-col items-center justify-center text-center">
+      <img
+        class="w-1/3"
+        src="../images/icons/63c7dc03818935e8280a6692_superthread-logo.svg"
+        alt=""
+      />
+      <p class="text-lg">&copy; 2020 - 2023. All rights reserved.</p>
+    </div>
+  </footer>
 </template>
